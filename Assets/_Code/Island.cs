3c75e69519d5dba1a9400f8ca9d6d190fc97c87a
Kamil Jamróz <nergydev@gmail.com>
2020-02-01 00:57:25 +0100

    stuff

--- Assets/_Code/Island.cs
+++ Assets/_Code/Island.cs
@@ -5,23 +5,56 @@ public class Island : MonoBehaviour
     [SerializeField]  Transform islandMergePoint;
     [SerializeField] Transform destination;
     [SerializeField] float timeToMergeInSeconds;
+    [SerializeField] Transform chunksParent;
+    [SerializeField] Material chunkMaterial;
 
     private Vector3 diff;
+    private bool mergeCompleted = false;
+    private Rigidbody[] rigidbodies;
 
     void Start()
     {
         diff = destination.position - islandMergePoint.position;
+
+        for (var i = 0; i < chunksParent.childCount; i++) {
+            var child = chunksParent.GetChild(i);
+            var meshCollider = child.gameObject.AddComponent<MeshCollider>();
+            meshCollider.convex = true;
+            
+            var rb = child.gameObject.AddComponent<Rigidbody>();
+            rb.isKinematic = true;
+
+            var r = child.GetComponent<MeshRenderer>();
+            if (r == null)
+                continue;
+            
+            r.sharedMaterial = chunkMaterial;
+        }
+        
+        rigidbodies = chunksParent.GetComponentsInChildren<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update() {
+        if (mergeCompleted) {
+            return;
+        }
+        
         var pos = transform.position;
 
         var currentDiff = destination.position - islandMergePoint.position;
-        if (currentDiff.magnitude > 0.01f) {
+        if (currentDiff.magnitude > 0.1f) {
             pos += diff * Time.deltaTime / timeToMergeInSeconds;
+        } else {
+            mergeCompleted = true;
+            StartFragmenting();
         }
 
         transform.position = pos;
+    }
+
+    void StartFragmenting() {
+        foreach (var r in rigidbodies) {
+            r.isKinematic = false;
+        }
     }
 }
