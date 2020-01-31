4dbbd3d5ce20a3e46fa11a84984ceabec75e5586
Kamil Jamróz <nergydev@gmail.com>
2020-01-31 23:15:19 +0100

    commit

--- Assets/_Code/Island.cs
+++ Assets/_Code/Island.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Island : MonoBehaviour
 {
+    [SerializeField] Direction direction;
+    [SerializeField] float floatingSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +11,23 @@ public class Island : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        var pos = transform.position;
+
+        switch (direction) {
+            case Direction.Left:
+                pos.x += Time.deltaTime * floatingSpeed;
+                break;
+            
+            case Direction.Right:
+                break;
+        }
+
+        transform.position = pos;
+    }
+
+    enum Direction {
+        Left,
+        Right
     }
 }
