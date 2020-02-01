01d587c4426a48cda6f948d9e82cf6bb12168c45
Patryk Czajka <czajka.patryk@empyrean.games>
2020-02-01 01:11:49 +0100

    proto camera system, looking at battle island for a few seconds

---
+++ Assets/_Code/DynamicCamera.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DynamicCamera : MonoBehaviour {
+    [SerializeField] float startPointLookDuration;
+    [SerializeField] float movementSpeed;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float backOffset;
+    [SerializeField] float upOffset;
+    [SerializeField] AnimationCurve curve;
+    [SerializeField] Transform startPoint;
+    [SerializeField] Transform startLookAtTarget;
+    [SerializeField] Transform player;
+    [SerializeField] Transform playerHead;
+    new Transform transform;
+    bool playerReached;
+
+    void Awake() {
+        transform = gameObject.transform;
+    }
+    IEnumerator Start() {
+        transform.position = startPoint.position;
+        transform.LookAt (startLookAtTarget);
+        yield return new WaitForSeconds (startPointLookDuration);
+        var progress = 0f;
+        while (progress < 1) {
+            var targetPosition = GetCameraTargetPosition();
+            progress += Time.deltaTime * curve.Evaluate ((targetPosition - transform.position).magnitude);
+            transform.position = Vector3.Lerp (transform.position, GetCameraTargetPosition(), progress);
+            var dir = (playerHead.position - targetPosition).normalized;
+            var lookRotation = Quaternion.LookRotation (dir);
+            transform.rotation = Quaternion.Slerp (transform.rotation, lookRotation, progress);
+            yield return null;
+        }
+
+        playerReached = true;
+    }
+
+    void Update() {
+        if(!playerReached)
+            return;
+        var targetPosition = GetCameraTargetPosition();
+        transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * movementSpeed);
+        var dir = (playerHead.position - targetPosition).normalized;
+        var lookRotation = Quaternion.LookRotation (dir);
+        transform.rotation = Quaternion.Slerp (transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    Vector3 GetCameraTargetPosition() {
+        return player.position - player.forward * backOffset + player.up * upOffset;
+    }
+}
