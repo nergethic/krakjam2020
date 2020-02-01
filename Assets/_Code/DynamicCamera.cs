46f7884f1b13242e86827e688fbf0c6e7fb942e4
Patryk Czajka <czajka.patryk@empyrean.games>
2020-02-01 04:00:54 +0100

    proto controller

--- Assets/_Code/DynamicCamera.cs
+++ Assets/_Code/DynamicCamera.cs
@@ -8,6 +8,8 @@ public class DynamicCamera : MonoBehaviour {
     [SerializeField] float backOffset;
     [SerializeField] float upOffset;
     [SerializeField] AnimationCurve curve;
+    [SerializeField] AnimationCurve deltaAngleToSpeedMultiplier;
+    [SerializeField] AnimationCurve deltaTranslationToSpeedMultiplier;
     [SerializeField] Transform startPoint;
     [SerializeField] Transform startLookAtTarget;
     [SerializeField] Transform player;
@@ -40,10 +42,13 @@ public class DynamicCamera : MonoBehaviour {
         if(!playerReached)
             return;
         var targetPosition = GetCameraTargetPosition();
-        transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * movementSpeed);
-        var dir = (playerHead.position - targetPosition).normalized;
-        var lookRotation = Quaternion.LookRotation (dir);
-        transform.rotation = Quaternion.Slerp (transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+        var dir = (playerHead.position - targetPosition);
+        var movSpeedMultiplier = deltaTranslationToSpeedMultiplier.Evaluate (dir.magnitude);
+        transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * movementSpeed * movSpeedMultiplier);
+
+        var lookRotation = Quaternion.LookRotation (dir.normalized);
+        var speedMultiplier = deltaAngleToSpeedMultiplier.Evaluate (Quaternion.Angle (transform.rotation, lookRotation));
+        transform.rotation = Quaternion.Slerp (transform.rotation, lookRotation, Time.deltaTime * rotationSpeed * speedMultiplier);
     }
 
     Vector3 GetCameraTargetPosition() {
---
+++ Assets/_Code/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [SerializeField] float walkSpeed = 1f;
+    [SerializeField] float runSpeed = 1.5f;
+    [SerializeField] float rotationSpeed = 1.5f;
+    [SerializeField] Animator animator;
+    const string SPEED_BLEND = "Speed";
+    const string JUMP_BOOL_NAME = "Jump";
+    const string JUMP_FORWARD_BOOL_NAME = "JumpForward";
+    new Transform transform;
+
+    void Awake() {
+        transform = gameObject.transform;
+    }
+    void Update() {
+        var shiftDown = Input.GetKey (KeyCode.LeftShift);
+        if (Input.GetKey (KeyCode.W)) {
+            animator.SetFloat(SPEED_BLEND, shiftDown ? 2 : 1);
+            transform.position += transform.forward * (shiftDown ? runSpeed : walkSpeed);
+            if (Input.GetKey (KeyCode.A))
+                transform.Rotate(0, -rotationSpeed, 0);
+            else if (Input.GetKey (KeyCode.D))
+                transform.Rotate(0, rotationSpeed, 0);
+            
+            if (Input.GetKeyDown (KeyCode.Space))
+                animator.SetTrigger (JUMP_FORWARD_BOOL_NAME);
+        }
+        else if(Input.GetKey (KeyCode.S)){
+            animator.SetFloat(SPEED_BLEND, -1);
+            transform.position -= transform.forward * walkSpeed/2f;
+            if (Input.GetKey (KeyCode.A))
+                transform.Rotate(0, rotationSpeed, 0);
+            else if (Input.GetKey (KeyCode.D))
+                transform.Rotate(0, -rotationSpeed, 0);
+        }
+        else {
+            animator.SetFloat (SPEED_BLEND, 0);
+        }
+
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            animator.SetTrigger (JUMP_BOOL_NAME);
+        }
+    }
+}
