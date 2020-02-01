22e04ff64f30b807548fefdfe0b67602c31f6afe
yurii <yurii@empyrean.games>
2020-02-01 21:03:39 +0100

    slider, meta bis

---
+++ Assets/_Code/UI/HPBar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Code.UI {
+    public class HPBar : MonoBehaviour {
+        [SerializeField] Transform fillTransform;
+        [SerializeField] Image fillImage;
+        [SerializeField] Image backgroundImage;
+
+        public Image FillImage => fillImage;
+        public Image BackgroundImage => backgroundImage;
+    
+        public void SetValue(float value) {
+            var currentScale = fillTransform.localScale;
+            currentScale.x = value;
+            fillTransform.localScale = currentScale;
+        }
+    }
+}
