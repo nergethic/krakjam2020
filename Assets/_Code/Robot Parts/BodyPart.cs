5118dc787b7c537df8b809b07ca7f365121c4f81
yurii <yurii@empyrean.games>
2020-02-01 05:15:19 +0100

    roboparts

--- Assets/_Code/Robot Parts/BodyPart.cs
+++ Assets/_Code/Robot Parts/BodyPart.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 namespace _Code.Robot_Parts {
     public class BodyPart : MonoBehaviour {
-        [SerializeField] BodySide side;
         [SerializeField] BodyType type;
+        [SerializeField] BodySide side;
         //ArmorPart placeholder;
         
         bool isOccupied;
@@ -14,9 +14,10 @@ namespace _Code.Robot_Parts {
         public bool IsOccupied => isOccupied;
         
         //void AttachArmor(ArmorPart armor){}
-    
+
         [Serializable]
-        public enum BodyType { Wrist, Glow, Shoulder, Helm, Chest, Boots }
-        public enum BodySide { Center, Right, Left} 
+        public enum BodyType { Wrist = 0, Shoulder = 1, Helm = 2, Chest = 3, Leg = 4 }
+        [Serializable]
+        public enum BodySide { Center = 0, Right = 2, Left = 3} 
     }
 }
--- Assets/_Code/Robot Parts/RobotBody.cs
+++ Assets/_Code/Robot Parts/RobotBody.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 namespace _Code.Robot_Parts {
     public class RobotBody : MonoBehaviour {
         [SerializeField] BodyPart[] bodyParts;
-        List<BodyPart.BodyType> centerBodyTypes = new List<BodyPart.BodyType> {BodyPart.BodyType.Chest, BodyPart.BodyType.Helm, BodyPart.BodyType.Boots};
+        List<BodyPart.BodyType> centerBodyTypes = new List<BodyPart.BodyType> {BodyPart.BodyType.Chest, BodyPart.BodyType.Helm, BodyPart.BodyType.Leg};
 
         public (bool, BodyPart) GetBodyPart(BodyPart.BodyType type) {
             foreach (var bodyPart in bodyParts) {
@@ -30,5 +29,29 @@ namespace _Code.Robot_Parts {
                     return (true, bodyPart);
             return (false, null);
         }
+        
+#if UNITY_EDITOR
+
+        [ContextMenu("Setup")]
+        void SetupPartsInsideRig() {
+            var namesToFind = new [] {"lowerArm", "head", "chest", "lowerleg"};
+            var parts = new List<BodyPart>();
+            foreach (var t in GetComponentsInChildren<Transform>()) {
+                AddFromList(namesToFind, t, parts);
+            }
+
+            bodyParts = parts.ToArray();
+        }
+
+        static void AddFromList(string[] namesToFind, Transform t, List<BodyPart> parts) {
+            foreach (var nameToFind in namesToFind) {
+                if (t.name.ToLower().Contains(nameToFind.ToLower()) && !t.name.ToLower().Contains("end".ToLower()) && t.GetComponent<BodyPart>() == null) {
+                    UnityEditor.Undo.RecordObject(t, "BodyPart");
+                    Debug.Log($"Adding part to {t.name}");
+                    parts.Add(t.gameObject.AddComponent<BodyPart>());
+                }
+            }
+        }
+#endif
     }
 }
