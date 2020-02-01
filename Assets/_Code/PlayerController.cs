b52443779b598cfe474dc12498674217cd4d9aa5
Patryk Czajka <czajka.patryk@empyrean.games>
2020-02-01 05:04:08 +0100

    final touch to camera

--- Assets/_Code/PlayerController.cs
+++ Assets/_Code/PlayerController.cs
@@ -1,30 +1,32 @@
 ﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    [SerializeField] float walkSpeed = 1f;
-    [SerializeField] float runSpeed = 1.5f;
-    [SerializeField] float rotationSpeed = 1.5f;
+    [SerializeField] float walkSpeed;
+    [SerializeField] float runSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] float rotationSpeed;
     [SerializeField] Animator animator;
+    [SerializeField] Rigidbody rb;
     const string SPEED_BLEND = "Speed";
-    const string JUMP_BOOL_NAME = "Jump";
+    const string JUMP_BLEND_NAME = "Jump";
     const string JUMP_FORWARD_BOOL_NAME = "JumpForward";
+    const float JUMP_COOLDOWN = 3f;
     new Transform transform;
+    float timeOfLastJump = -3f;
 
     void Awake() {
         transform = gameObject.transform;
     }
     void Update() {
         var shiftDown = Input.GetKey (KeyCode.LeftShift);
-        if (Input.GetKey (KeyCode.W)) {
+        var forwardKeyPressed = Input.GetKey (KeyCode.W);
+        if (forwardKeyPressed) {
             animator.SetFloat(SPEED_BLEND, shiftDown ? 2 : 1);
             transform.position += transform.forward * (shiftDown ? runSpeed : walkSpeed);
             if (Input.GetKey (KeyCode.A))
                 transform.Rotate(0, -rotationSpeed, 0);
             else if (Input.GetKey (KeyCode.D))
                 transform.Rotate(0, rotationSpeed, 0);
-            
-            if (Input.GetKeyDown (KeyCode.Space))
-                animator.SetTrigger (JUMP_FORWARD_BOOL_NAME);
         }
         else if(Input.GetKey (KeyCode.S)){
             animator.SetFloat(SPEED_BLEND, -1);
@@ -38,8 +40,10 @@ public class PlayerController : MonoBehaviour {
             animator.SetFloat (SPEED_BLEND, 0);
         }
 
-        if (Input.GetKeyDown (KeyCode.Space)) {
-            animator.SetTrigger (JUMP_BOOL_NAME);
+        if (Input.GetKeyDown (KeyCode.Space) && Time.time > timeOfLastJump + JUMP_COOLDOWN) {
+            animator.SetTrigger (forwardKeyPressed ? JUMP_FORWARD_BOOL_NAME : JUMP_BLEND_NAME);
+            rb.AddForce (Vector3.Lerp (transform.forward, Vector3.up, 0.5f) * jumpForce, ForceMode.Impulse);
+            timeOfLastJump = Time.time;
         }
     }
 }
