0d0dc2dd54e394a5919fd34da256f3fb0c863177
piorotorus <piotr.pochopien@microsoft.wsei.edu.pl>
2020-02-02 04:30:23 +0100

    add audio

--- Assets/_Code/Audio/Sound.cs
+++ Assets/_Code/Audio/Sound.cs
@@ -10,6 +10,8 @@ public class Sound : ScriptableObject
     public AudioClip clip;
     [Range(0f, 1f)]
     public float volume;
+
+    public float pitch=1;
     [HideInInspector]
     public AudioSource source;
 }
